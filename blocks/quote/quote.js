3fbc58fcb87ae258e40331c8ba75c1a6f568a23b
Dawid Pura <puradawid@gmail.com>
2024-11-18 20:20:23 +0100

    Remove lint concenrs from quote component

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,4 +1,4 @@
 export default async function decorate(block) {
   const [quote, author] = block.children;
-  block.innerHTML = `<blockquote>${quote.innerText}</blockquote><div class=author>Author: ${author.innerText}</div>`
+  block.innerHTML = `<blockquote>${quote.innerText}</blockquote><div class=author>Author: ${author.innerText}</div>`;
 }
